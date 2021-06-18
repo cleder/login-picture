32ed472dc3124fe8f9f1d9d5336fc626bed5c29a
Christian Ledermann <christian.ledermann@blueface.com>
2021-06-18 10:35:52 +0100

    add face detection

--- detcap.py
+++ detcap.py
@@ -31,17 +31,33 @@ vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 10_000)
 cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+haar_face_cascade = cv2.CascadeClassifier('./venv/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
 while True:
 
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
+    flipped = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(flipped, cv2.COLOR_BGR2GRAY)
+    detected = False
+
+    faces = haar_face_cascade.detectMultiScale(gray, 1.1, 3, minSize=(140, 140))
+    for x, y, w, h in faces:
+        cv2.rectangle(flipped, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        text_size, _ = cv2.getTextSize(f'Face {w}x{h}', cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        cv2.rectangle(flipped, (x, y - text_size[1]), (x + text_size[0], y), (255, 255, 255), cv2.FILLED)
+        cv2.putText(flipped, f'Face {w}x{h}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        detected = True
 
     # Display the resulting frame
-    cv2.imshow("frame", frame)
+    cv2.imshow("frame", flipped)
 
+    key = cv2.waitKey(30)
+    if not detected:
+        continue
     # press any key to exit
-    if cv2.waitKey(50) > 0:
+    if  key > 0:
         cv2.imwrite(filename(), frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
         break
 
