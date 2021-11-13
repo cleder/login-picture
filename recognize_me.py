9145caa76fdb4d88ef05fd01be001a9850e2ab4d
Christian Ledermann <christian.ledermann@gmail.com>
2021-11-13 16:03:39 +0000

    refactor into smaller functions

--- recognize_me.py
+++ recognize_me.py
@@ -3,23 +3,35 @@ import datetime
 import getpass
 import pathlib
 import pickle
+from functools import partial
+from typing import Callable, List, Optional, Tuple
 
 import cv2
 import face_recognition
+from numpy import ndarray
 
 DETECTION_METHOD = "cnn"
-path = pathlib.Path.home() / "Pictures" / "login-capture"
 
 
-def load_encodings(path: pathlib.Path):
+def get_image_path() -> pathlib.Path:
+    """
+    Get the path of the directory where the images are stored.
+
+    Default: ``~/Pictures/login-capture/``.
+    """
+    return pathlib.Path.home() / "Pictures" / "login-capture"
+
+
+def load_encodings(path: pathlib.Path) -> List[ndarray]:
+    """Load pickle file containing face encodings."""
     encodings = []
     for encoding in path.glob(f"*.jpg.{DETECTION_METHOD}-encoded.pickle"):
-        data = pickle.loads(open(str(encoding), "rb").read())
-        encodings.append(data)
+        with open(encoding, "rb") as f:
+            encodings.append(pickle.load(f))
     return encodings
 
 
-def filename() -> str:
+def get_filename() -> str:
     """
     Create a filename based on the username and current date and time.
 
@@ -27,45 +39,52 @@ def filename() -> str:
     """
     username = getpass.getuser()
     now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-
-    path = pathlib.Path.home() / "Pictures" / "login-capture"
+    path = get_image_path()
     path.mkdir(parents=True, exist_ok=True)
-
     return str(path / f"{username}-{now}.jpg")
 
 
-data = load_encodings(path)
-# define a video capture object
-vid = cv2.VideoCapture(0)
-# set the resolution to the maximum value
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, 10_000)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 10_000)
+def create_window() -> str:
+    """Create an output window and set it to full screen."""
+    window_name = "Recognize me"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_TOPMOST, 1)
+    return window_name
 
-# create an output window and set it to full screen
-cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-datapath = (
-    pathlib.Path(__file__).resolve().parents[0]
-    / "data"
-    / "haarcascade_frontalface_default.xml"
-)
-haar_face_cascade = cv2.CascadeClassifier(str(datapath))
+def get_camera_capture(ratio: Optional[float] = 3) -> Tuple[cv2.VideoCapture, int]:
+    """Define a video capture object."""
+    vid = cv2.VideoCapture(0)
+    # set the resolution to the maximum value
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 10_000)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 10_000)
+    w = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    return vid, int(min(w, h) / ratio)
 
-w = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-h = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-min_size = int(min(w, h) / 3)
-while vid.isOpened():
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-    flipped = cv2.flip(frame, 1)
+
+def get_face_detector(min_size: int) -> Callable:
+    """Create a face detection classifier and return a detection function."""
+    datapath = (
+        pathlib.Path(__file__).resolve().parents[0]
+        / "data"
+        / "haarcascade_frontalface_default.xml"
+    )
+    haar_face_cascade = cv2.CascadeClassifier(str(datapath))
+    return partial(
+        haar_face_cascade.detectMultiScale,
+        scaleFactor=1.1,
+        minNeighbors=3,
+        minSize=(min_size, min_size),
+    )
+
+
+def detect_face(flipped: ndarray, detector: Callable) -> ndarray:
+    """Detect faces in an image and draw a bounding box around them."""
     gray = cv2.cvtColor(flipped, cv2.COLOR_BGR2GRAY)
     detected = False
-
-    faces = haar_face_cascade.detectMultiScale(
-        gray, 1.1, 3, minSize=(min_size, min_size)
-    )
+    faces = detector(gray)
     for x, y, w, h in faces:
         cv2.rectangle(flipped, (x, y), (x + w, y + h), (0, 255, 0), 3)
         text_size, _ = cv2.getTextSize(f"Face {w}x{h}", cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
@@ -81,13 +100,15 @@ while vid.isOpened():
         )
         detected = True
 
-    # Display the resulting frame
-    cv2.imshow("frame", flipped)
+    return detected, flipped
 
-    key = cv2.waitKey(30)
-    if not detected:
-        continue
 
+def recognize_face(data: List[ndarray], frame: ndarray) -> Tuple[int, ndarray]:
+    """
+    Recognize a face in an image.
+
+    Return the number of matches and the encodings.
+    """
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     boxes = face_recognition.face_locations(rgb, model=DETECTION_METHOD)
     encodings = face_recognition.face_encodings(rgb, boxes, model="large")
@@ -96,26 +117,52 @@ while vid.isOpened():
     for encoding in encodings:
         # attempt to match each face in the input image to our known
         # encodings
+        # tolerance: How much distance between faces to consider it a match.
+        # Lower is more strict. 0.6 is typical best performance.
         matches = face_recognition.compare_faces(
             known_face_encodings=data,
             face_encoding_to_check=encoding,
             tolerance=0.4,
         )
-    if not any(matches):
-        print("no match")
+    return len([m for m in matches if m]), encodings
+
+
+def save_image_and_encoding(frame: ndarray, encodings: List[ndarray]) -> None:
+    """Save an image and its encoding to disk."""
+    filename = get_filename()
+    cv2.imwrite(filename, frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    # Write the encoding only when exactly one face was detected
+    if len(encodings) == 1:
+        with open(f"{filename}.{DETECTION_METHOD}-encoded.pickle", "wb") as encoded:
+            pickle.dump(encodings[0], encoded)
+
+
+data = load_encodings(get_image_path())
+vid, min_size = get_camera_capture()
+window_name = create_window()
+face_detector = get_face_detector(min_size)
+
+while vid.isOpened():
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+    # Flip the frame so that it is the mirror view
+    flipped = cv2.flip(frame, 1)
+    # detect faces in the flipped frame
+    detected, flipped = detect_face(flipped, face_detector)
+    # Display the resulting frame
+    cv2.imshow(window_name, flipped)
+    key = cv2.waitKey(30)
+    if not detected:
+        continue
+    matches, encodings = recognize_face(data, frame)
+    if not matches:
+        print("No face recognized")
         continue
     # save the image once a face was recognized.
-    num_matches = len([m for m in matches if m])
-    print(f"{num_matches} matches")
-    fname = filename()
-    cv2.imwrite(fname, frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
-    # Write the encoding only when exactly one face was detected
-    if len(boxes) == 1:
-        with open(f"{fname}.{DETECTION_METHOD}-encoded.pickle", "wb") as encoded:
-            encoded.write(pickle.dumps(encoding))
-    break
+    save_image_and_encoding(frame, encodings)
+    print(f"{matches} matches")
+    # release the camera, this will stop the video capture and exit the loop
+    vid.release()
 
-# After the loop release the cap object
-vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
